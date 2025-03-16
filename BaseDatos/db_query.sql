8071242e2e4ea8089a03e91164c9630d0181687a
Harlyn Luna <harlynlunab@gmail.com>
2025-03-16 12:50:00 -0600

    Creación de la base de datos
    Se realiza el desarrollo y estructuración de la base de datos con:
    
    Tablas
    Inserts
    Claves primarias
    Claves foráneas
    Procedimientos almacenados

---
+++ BaseDatos/db_query.sql
@@ -0,0 +1,105 @@
+-- Prueba Tecnica - Harlyn Luna Brenes
+
+-- Elimnar base de datos si existe
+-- DROP DATABASE IF EXISTS ColegioBD;
+
+-- Creación de la base de datos ColegioDB
+CREATE DATABASE ColegioBD;
+
+-- Tabla de Estudiantes
+CREATE TABLE ColegioBD.ESTUDIANTE (
+    IdEstudiante INT PRIMARY KEY AUTO_INCREMENT,
+    NombreEstudiante VARCHAR(50) NOT NULL,
+    CorreoEstudiante VARCHAR(50) NOT NULL, 
+    FechaCreacion DATETIME
+);
+
+-- Tabla de Cursos
+CREATE TABLE ColegioBD.CURSOS(
+    idCurso INT PRIMARY KEY AUTO_INCREMENT,
+    NombreCurso VARCHAR(50) NOT NULL
+);
+
+-- Tabla de Estudiantes_Cursos
+CREATE TABLE ColegioBD.ESTUDIANTE_CURSO(
+    IdEstudiante INT NOT NULL,
+    IdCurso INT NOT NULL,
+    PRIMARY KEY(IdEstudiante, IdCurso),
+    FOREIGN KEY(IdEstudiante) REFERENCES ESTUDIANTE(IdEstudiante),
+    FOREIGN KEY(IdCurso) REFERENCES CURSOS(idCurso)
+);
+
+-- Insertar Datos Tabla Estudiantes
+INSERT INTO ColegioBD.ESTUDIANTE (NombreEstudiante, CorreoEstudiante, FechaCreacion) VALUES ('Harlyn','hluna@edu.co.cr', NOW());
+INSERT INTO ColegioBD.ESTUDIANTE (NombreEstudiante, CorreoEstudiante, FechaCreacion) VALUES ('Luis','lsolis@edu.co.cr', NOW());
+INSERT INTO ColegioBD.ESTUDIANTE (NombreEstudiante, CorreoEstudiante, FechaCreacion) VALUES ('Maria','mrodriguez@edu.co.cr', NOW());
+
+-- Insertar Datos Tabla Cursos
+INSERT INTO ColegioBD.CURSOS (NombreCurso) VALUES ('Matematicas');
+INSERT INTO ColegioBD.CURSOS (NombreCurso) VALUES ('Español');
+INSERT INTO ColegioBD.CURSOS (NombreCurso) VALUES ('Ciencias');
+INSERT INTO ColegioBD.CURSOS (NombreCurso) VALUES ('Estudios Sociales');
+
+-- Insertar Datos Tabla Estudiantes_Cursos
+INSERT INTO ColegioBD.ESTUDIANTE_CURSO (IdEstudiante, IdCurso) VALUES (1, 1);
+INSERT INTO ColegioBD.ESTUDIANTE_CURSO (IdEstudiante, IdCurso) VALUES (1, 3);
+INSERT INTO ColegioBD.ESTUDIANTE_CURSO (IdEstudiante, IdCurso) VALUES (2, 2);
+INSERT INTO ColegioBD.ESTUDIANTE_CURSO (IdEstudiante, IdCurso) VALUES (3, 4);
+INSERT INTO ColegioBD.ESTUDIANTE_CURSO (IdEstudiante, IdCurso) VALUES (3, 1);
+
+-- Procedimientos almacenados 
+
+-- Procedimiento para obtener estudiantes
+DELIMITER $$
+CREATE PROCEDURE ColegioBD.P_ObtenerEstudiantes()
+BEGIN
+    SELECT * FROM ESTUDIANTE;
+END $$
+DELIMITER ;
+
+-- Procedimiento para obtener estudiante por ID
+DELIMITER $$
+CREATE PROCEDURE ColegioBD.P_ObtenerEstudianteID(
+    IN c_IdEstudiante INT
+)
+BEGIN
+    SELECT * FROM ESTUDIANTE WHERE IdEstudiante = c_IdEstudiante;
+END $$
+DELIMITER ;
+
+-- Procedimiento para editar estudiante
+DELIMITER $$
+CREATE PROCEDURE ColegioBD.P_EditarEstudiantes(
+    IN c_IdEstudiante INT,
+    IN v_NombreEstudiante VARCHAR(50),
+    IN v_CorreoEstudiante VARCHAR(50)
+)
+BEGIN
+    UPDATE Estudiante
+    SET 
+        NombreEstudiante = v_NombreEstudiante,
+        CorreoEstudiante = v_CorreoEstudiante
+    WHERE IdEstudiante = c_IdEstudiante;
+END $$
+DELIMITER ;
+
+-- Procedimiento para eliminar estudiantes
+DELIMITER $$
+CREATE PROCEDURE ColegioBD.P_EliminarEstudianteID(
+    IN c_IdEstudiante INT
+)
+BEGIN
+    DELETE FROM ESTUDIANTE WHERE IdEstudiante = c_IdEstudiante;
+END $$
+DELIMITER ;
+
+-- Procedimiento para agregar nuevo Estudiante
+DELIMITER $$
+CREATE PROCEDURE ColegioBD.P_AgregarEstudiante(
+    IN v_NombreEstudiante VARCHAR(50),
+    IN v_CorreoEstudiante VARCHAR(50)
+)
+BEGIN
+    INSERT INTO ESTUDIANTE (NombreEstudiante, CorreoEstudiante, FechaCreacion) VALUES (v_NombreEstudiante, v_CorreoEstudiante, NOW());
+END $$
+DELIMITER ;
